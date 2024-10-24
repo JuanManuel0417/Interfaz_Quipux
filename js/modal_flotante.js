1ad7d7ddb8f4fa88684bc983d253fab59f33a1b5
JuanManuel0417 <gonzalezmejiaj264@gmail.com>
2024-10-24 14:21:57 -0500

    Se elimanron los elementos responsivos ya que quedaron mal, y se cambio la funcionalidad del archio js

--- js/modal_flotante.js
+++ js/modal_flotante.js
@@ -1,13 +1,18 @@
+// Funcion para abrir el modal y mostrar el nombre del estudiante en el mensaje
 function openModal(nombreEstudiante) {
     document.getElementById('modal_text').innerText = `Eliminar ${nombreEstudiante}`;
     document.getElementById('modal').style.display = 'block';
 }
 
+// Función para cerrar el modal
 function closeModal() {
     document.getElementById('modal').style.display = 'none';
 }
 
+// Funcion para mostar mensaje en caso de que se de al boton "Si, eliminar" 
 function confirmDelete() {
+    // Cierra el modal despues de confirmar la eliminacion
     closeModal();
+    // Muestra una alerta indicando que el estudiante ha sido eliminado
     alert('Estudiante eliminado');
 }
